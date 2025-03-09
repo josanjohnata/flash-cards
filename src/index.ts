8de12c693c37567d9f2400663c5ada8e18fc694d
John Pontes <jpontes@questrade.com>
2025-03-09 12:10:53 -0300

    feat: Finished the setup of API

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,10 @@ const { PORT } = process.env || 3000;
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.json({ ok: true })
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
