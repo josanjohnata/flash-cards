94c3d3e74cce2a1baee58045751f4e7ed9af21f3
John Pontes <jpontes@questrade.com>
2025-03-09 11:41:28 -0300

    feat: Inicia a API

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const { PORT } = process.env || 3000;
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
